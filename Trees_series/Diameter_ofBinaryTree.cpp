f7bf8f965405412ceb7ee8815bd0030197070b08
parth506 <parthagrawal4675@gmail.com>
2025-05-30 23:34:21 +0530

    new problem added

---
+++ Trees_series/Diameter_ofBinaryTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxi = 0;
+    int check(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int l = check(root->left);
+        int r = check(root->right);
+        maxi = max(maxi, l + r);
+        return 1 + max(l, r);
+    }
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+
+        check(root);
+        return maxi;
+    }
+};
