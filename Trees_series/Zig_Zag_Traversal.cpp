3eabbb8c9969fbaf7f56e2499986f50d3e753e8d
parth506 <parthagrawal4675@gmail.com>
2025-05-31 11:20:05 +0530

    changes made in the zigzag traversal problem

---
+++ Trees_series/Zig_Zag_Traversal.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    vector<vector<int>> zigZagTraversal(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        bool leftToRight = true;
+        while (!q.empty())
+        {
+            int size = q.size();
+            vector<int> row(size); // improttant to add the size
+            for (int i = 0; i < size; ++i)
+            {
+                int index = leftToRight ? i : size - 1 - i;
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left != nullptr)
+                {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr)
+                {
+                    q.push(node->right);
+                }
+                row[index] = node->val;
+            }
+            leftToRight = !leftToRight;
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
+
+// Function to build tree from level-order input
+TreeNode *buildTree()
+{
+    int val;
+    cin >> val;
+    if (val == -1)
+        return nullptr;
+
+    TreeNode *root = new TreeNode(val);
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        TreeNode *node = q.front();
+        q.pop();
+
+        int leftVal, rightVal;
+        cin >> leftVal;
+        if (leftVal != -1)
+        {
+            node->left = new TreeNode(leftVal);
+            q.push(node->left);
+        }
+
+        cin >> rightVal;
+        if (rightVal != -1)
+        {
+            node->right = new TreeNode(rightVal);
+            q.push(node->right);
+        }
+    }
+
+    return root;
+}
+
+int main()
+{
+
+    TreeNode *root = buildTree();
+
+    Solution sol;
+    int depth = sol.maxDepth(root);
+    cout << depth << endl;
+
+    return 0;
+}
