8a7c9a9c1df58a6b00fff9b1e183ba755cd85ffd
parth506 <parthagrawal4675@gmail.com>
2025-07-01 20:36:55 +0530

    daily problem added

---
+++ leetcode_daily_problem_solving/3330.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        unordered_map<int,int> m;
+        int sum=0;
+        for(int i=0 ; i < word.size()-1 ;++i){
+             
+             if(word[i]==word[i+1])
+             m[word[i]-'a']++;
+             
+        }
+        for(auto i : m){
+           
+                sum=sum+i.second;
+        }
+        return sum+1;
+    }
+};
