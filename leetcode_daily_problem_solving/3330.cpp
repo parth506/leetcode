30630dbf8962f276d04c96ddf495f409e7e37cd4
parth506 <parthagrawal4675@gmail.com>
2025-07-01 20:39:08 +0530

    changes made in daily problem

--- leetcode_daily_problem_solving/3330.cpp
+++ leetcode_daily_problem_solving/3330.cpp
@@ -1,18 +1,23 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    int possibleStringCount(string word) {
-        unordered_map<int,int> m;
-        int sum=0;
-        for(int i=0 ; i < word.size()-1 ;++i){
-             
-             if(word[i]==word[i+1])
-             m[word[i]-'a']++;
-             
+    int possibleStringCount(string word)
+    {
+        unordered_map<int, int> m;
+        int sum = 0;
+        for (int i = 0; i < word.size() - 1; ++i)
+        {
+
+            if (word[i] == word[i + 1])
+                m[word[i] - 'a']++;
         }
-        for(auto i : m){
-           
-                sum=sum+i.second;
+        for (auto i : m)
+        {
+
+            sum = sum + i.second;
         }
-        return sum+1;
+        return sum + 1;
     }
 };
